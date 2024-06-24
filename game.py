ea2d2eac2acb90f4c0690919503a23d68a2d2f83
Samir Paudyal <samirpau4@gmail.com>
2024-06-24 12:44:20 -0500

    Added continuos movement and character transformation

--- game.py
+++ game.py
@@ -8,28 +8,28 @@ X = 1168
 Y = 655
 BORDER_SIZE = 10
 AVATAR_SIZE = 60
-VEL = 190
+VEL = 4
 AVATAR_X = (X - AVATAR_SIZE) / 2 # 4
 AVATAR_Y = (Y - AVATAR_SIZE) / 2 # 6
 
 
 direction = pygame.K_RIGHT
-
-character_selection = input("Do you want to be Jack or Michael? ")
-avatar_filepath = 'Jack.png'
-if character_selection == "Jack" or "jack":
-    avatar_filepath = 'Jack.png' if direction != pygame.K_LEFT else 'Jack_Left.png'
-elif character_selection == "Michael" or "michael":
-    avatar_filepath = 'Michael.png' if direction != pygame.K_RIGHT else 'Michael_Left.png'
-# Loading Jack.png(the character) and making it transparent.
-img = pygame.image.load(avatar_filepath).convert_alpha()
-
-# Scaling the character so that is fits according to my size in the frame.
-img = pygame.transform.scale(img, (AVATAR_SIZE, AVATAR_SIZE))
+key_pressed = False
 
 screen = pygame.display.set_mode((X, Y))
 clock = pygame.time.Clock()
 running = True
+
+avatar_filepath = 'Jack.png'
+avatar = pygame.image.load(avatar_filepath).convert_alpha()
+avatar = pygame.transform.scale(avatar, (AVATAR_SIZE, AVATAR_SIZE))
+
+avatar_filepath_left = 'Jack_Left.png'
+avatar_left = pygame.image.load(avatar_filepath_left).convert_alpha()
+avatar_left = pygame.transform.scale(avatar_left, (AVATAR_SIZE, AVATAR_SIZE))
+# Loading Jack.png(the character) and making it transparent.
+
+# Scaling the character so that is fits according to my size in the frame.
 
 while running == True:
     
@@ -45,28 +45,34 @@ while running == True:
     
     # Putting the character in the middle
     # and overlapping the two rectangles that we made earlier plus the character.
-    screen.blit(img, midpoint)
+    screen.blit(avatar_left if direction == pygame.K_LEFT else avatar, midpoint)
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.KEYUP and pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN:
             direction = event.key
-            if event.key == pygame.K_LEFT:
-                AVATAR_X -= VEL
-                time.sleep(5)
-            if event.key == pygame.K_RIGHT:
-                AVATAR_X += VEL
-            if event.key == pygame.K_UP:
-                AVATAR_Y -= VEL
-            if event.key == pygame.K_DOWN:
-                AVATAR_Y += VEL
+            key_pressed = True
+        if event.type == pygame.KEYUP:
+            key_pressed = False
+
+    if key_pressed == True:
+        if event.key == pygame.K_LEFT:
+            AVATAR_X -= VEL
+        if event.key == pygame.K_RIGHT:
+            AVATAR_X += VEL
+        if event.key == pygame.K_UP:
+            AVATAR_Y -= VEL
+        if event.key == pygame.K_DOWN:
+            AVATAR_Y += VEL
     
     AVATAR_X = min(X - AVATAR_SIZE, AVATAR_X)
     AVATAR_Y = min(Y - AVATAR_SIZE, AVATAR_Y)
     AVATAR_X = max(0, AVATAR_X)
     AVATAR_Y = max(0, AVATAR_Y)
     pygame.display.flip()
+    
+    clock.tick(60)
     
     
 pygame.quit()
