4533f08cff6c15ae8727dd92236144a011c3815b
Samir Paudyal <samirpau4@gmail.com>
2024-07-13 13:20:28 -0500

    Saved constants

--- constants.py
+++ constants.py
@@ -4,7 +4,5 @@ Y = 552
 BORDER_SIZE = 10
 AVATAR_SIZE = 80
 VEL = 4
-AVATAR_X = (X - AVATAR_SIZE) / 2 # 4
-AVATAR_Y = (Y - AVATAR_SIZE) / 2 # 6
 ASSET_FOLDER = 'Assets/'
 HOUSE_FILEPATH = 'Assets/House_background.png'
